91505de65664388e2adeef2b73443f47009ddbca
Artem <artempolzikov25@gmail.com>
2025-03-06 17:26:08 +0300

    Rebuild TgBot init commit

---
+++ src/main/java/pro/sky/CW_TgBot/CwTgBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CW_TgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/CW_TgBot/CwTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CW_TgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
